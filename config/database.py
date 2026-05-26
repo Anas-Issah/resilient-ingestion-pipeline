d12d37794c7fccf3b96b7f6f2e4de1d906b2c1e4
Anas Issah <anasisa01@gmail.com>
2026-05-26 01:45:48 +0000

    initial database.py and extract.py commit

--- config/database.py
+++ config/database.py
@@ -1,1 +1,27 @@
+from sqlalchemy import MetaData, URL,Table,inspect,create_engine,select,text
+from dotenv import load_dotenv
+import os
+load_dotenv(override=True)
+def db_connector():
+    url = URL.create(
+        "postgresql+psycopg2",
+        username=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME")
+    )
 
+    meta = MetaData()
+    engine = create_engine(url=url,pool_size=5,pool_timeout=5,pool_recycle=300,pool_pre_ping=True)
+    with engine.connect() as conn:
+        conn.execute(text("CREATE TABLE IF NOT EXIST coin_tb (" \
+        "id VARCHAR(10) PRIMARY KEY NOT NULL,name VARCHAR(50)) NOT NULL"))
+
+        conn.execute(text("CREATE TABLE IF NOT EXIST coin_detail_tb(" \
+        "current_price NUMERIC(18,2) NOT NULL,high_24h NUMERIC(18,2) NOT NULL," \
+        "low_24h NUMERIC(18,2) NOT NULL,market_cap NUMERIC(18,2) NOT NULL, total_volume NUMERIC(20,2) NOT NULL ," \
+        " last_updated TIMESTAMPTZ NOT NULL"))
+        
+    coin_tb = Table("coin_tb",meta,autoload_with=engine)
+    coin_detail_tb = Table("coin_detail_tb",meta,autoload_with=engine)
+    return [engine, coin_tb, coin_detail_tb ]
--- pipelines/extract.py
+++ pipelines/extract.py
@@ -1,1 +1,30 @@
+import os,requests
+from dotenv import load_dotenv
+load_dotenv()
 
+
+def extrator()-> list:
+    key = os.getenv("CG_key")
+    headers ={
+        "accept":"application/json",
+        "x_cg_demo_api_key":key
+
+    }
+    params = {
+        "_page":1,
+        "per_page":20,
+        "vs_currency":'usd',
+        "order":"market_cap_rank_asc"
+    }
+    url = f"https://api.coingecko.com/api/v3/coins/markets"
+
+    response = requests.get(url=url,headers=headers,params=params)
+    
+    return [
+
+        [{"id":coin["symbol"],"name":coin["name"]}  for coin in response.json()],
+         [   {"current_price":coin["current_price"],"market_cap":coin["market_cap"],
+             'high_24h':coin["high_24h"],"low_24h":coin["low_24h"], "total_volume":coin["total_volume"],
+             'last_upated':coin["last_updated"]}
+             for coin in response.json()]
+    ]
