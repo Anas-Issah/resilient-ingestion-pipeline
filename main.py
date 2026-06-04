a184162691a60d2aff2c1ef597ad3db3448e82a1
Anas Issah <anasisa01@gmail.com>
2026-06-04 01:21:49 +0000

    added decorator to error_handler file

--- main.py
+++ main.py
@@ -7,41 +7,51 @@ from config import database
 from utils import auditor, error_handler
 
 load_dotenv()
+# 
+retry = 3
+# 
+try:
+    extracted_data = extract.extrator()
+    if not type(extracted_data) == list:
+        if 400 <= extracted_data and 500 > extracted_data:
+            if extracted_data == 429:
+                raise error_handler.TransientError(f"{extracted_data}: Rate limit error.Implementing backoff retry")
+            if extracted_data == 401 or extracted_data == 403:
+                raise error_handler.AuthValidationError(f"{extracted_data}: Authentication or validation error encountered:Pipeline stopped")
+            else:
+                raise error_handler.FatalError(f"{extracted_data}: Fatal network error occured.Pipeline stopped")
+        else:
+            if extracted_data == 503:
+                raise error_handler.TransientError(f"{extracted_data}:backoff delay emplemented")
+            else :
+                raise error_handler.TransientError(f"{extracted_data}:Retrying connecting to source - retries left {retry}")
+            # 
+    else:
+        stored_coin_ids = [coin["id"] for coin in extracted_data[0]]
+        transformed_data = transform.transform_data(stored_coin_ids, extracted_data)
+        # 
+        #connect to database
+        db_connector = database.db_connector()
+        if db_connector == 0:
+            raise error_handler.FatalError(f"Programming or Interface error during database connection: Pipeline stopped!")
+        elif db_connector == 1:
+            retry-= 1
+            raise error_handler.TransientError(f"A transient error occured during database connection: retries left {retry}")
+        else:
+            # audit database
+            auditor.coin_tb_auditor(coin_tb=extracted_data[1],engine=extracted_data[0])
+            auditor.coin_details_tb_auditor(coin_details_tb=extracted_data[2],engine=extracted_data[0])
+            load.loader(connector=extracted_data[:3],transformed_data=transformed_data)
+# 
+except error_handler.FatalError as e:
+    print(e)
+except error_handler.AuthValidationError as e:
+    print(e)
+# 
 
-# retry = 3
 
-# try:
-#     extracted_data = extract.extrator()
-#     if not type(extracted_data) == list:
-#         if 400 <= extracted_data and 500 > extracted_data:
-#             if extracted_data == 429:
-#                 raise error_handler.TransientNetworkError(f"{extracted_data}: Rate limit error.Implementing backoff retry")
-#             if extracted_data == 401 or extracted_data == 403:
-#                 raise error_handler.AuthValidationError(f"{extracted_data}: Authentication or validation error encountered:Pipeline stopped")
-#             else:
-#                 raise error_handler.FatalNetworkError(f"{extracted_data}: Fatal network error occured.Pipeline stopped")
-#         else:
-#             if extracted_data == 503:
-#                 raise error_handler.TransientNetworkError(f"{extracted_data}:backoff delay emplemented")
-#             else :
-#                 raise error_handler.TransientNetworkError(f"{extracted_data}:Retrying connecting to source")
-            
-#     else:
-#         stored_coin_ids = [coin["id"] for coin in extracted_data[0]]
-#         transformed_data = transform.transform_data(stored_coin_ids, extracted_data)
-        
-#         #connect to database
-#         db_connector = database.db_connector()
-
-# except error_handler.FatalNetworkError as e:
-#     print(e)
-# except error_handler.AuthValidationError as e:
-#     print(e)
-
-
-
-db = database.db_connector()
-print(db)
+# db = database.db_connector()
+# print(db)
 # from sqlalchemy import select
 
 # extracted_data = extract.extrator()
--- utils/error_handler.py
+++ utils/error_handler.py
@@ -1,6 +1,87 @@
+from logging.handlers import RotatingFileHandler
+import logging
+
 
 class FatalError(Exception):pass
 
-class TransientError(Exception):pass
+class TransientErrorBackoff(Exception):pass
 
 class AuthValidationError(Exception):pass
+
+class TransientErrorRetry(Exception):pass
+
+logger = logging.getLogger(__name__)
+handler = RotatingFileHandler(
+    "pipeline.log",
+    maxBytes=4_000_000,
+    backupCount=5
+
+)
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+)
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+
+def run_pipeline(func1,func2,func3,func4,func5,func6):
+    def decorator(func):
+        def wrapper(*args, **kwags):
+            pipe_running = True
+            while pipe_running:
+                try:
+                    retry = 3
+                    back_of = 1
+                    extracted_data = func1.extractor()
+                    if not type(extracted_data) == list:
+                        if 400 <= extracted_data and 500 > extracted_data:
+                            if extracted_data == 429:
+                                back_of *= 2
+                                raise TransientErrorBackoff(f"{extracted_data}: Rate limit error.Implementing backoff retry.Backoff time {back_of} seconds")
+                            elif extracted_data == 401 or extracted_data == 403:
+                                raise AuthValidationError(f"{extracted_data}: Authentication or validation error encountered:Pipeline stopped")
+                            else:
+                                raise FatalError(f"{extracted_data}: Fatal network error occured.Pipeline stopped")
+                        else:
+                            if extracted_data == 503:
+                                back_of *= 2
+                                raise TransientErrorBackoff(f"{extracted_data}:backoff delay implemented")
+                            else :
+                                retry -= 1
+                                raise TransientErrorRetry(f"{extracted_data}:Retrying connecting to source - retries left {retry}")
+                    else:
+                        # get coin ids and transform data to required format
+                        stored_coin_ids = [coin["id"] for coin in extracted_data[0]]
+                        transformed_data = func2.transform_data(stored_coin_ids, extracted_data)
+
+                        #connect to database
+                        db_connector = func3.db_connector()
+                        if db_connector == 0:
+                            raise FatalError(f"Programming or Interface error during database connection: Pipeline stopped!")
+
+                        elif db_connector == 1:
+                            retry-= 1
+                            raise TransientErrorRetry(f"A transient error occured during database connection: retries left {retry}")
+                        else:
+                            # audit database
+                            func4.coin_tb_auditor(coin_tb=extracted_data[1],engine=extracted_data[0])
+                            func5.coin_details_tb_auditor(coin_details_tb=extracted_data[2],engine=extracted_data[0])
+                            #load data
+                            func6.loader(connector=extracted_data[:3],transformed_data=transformed_data)
+                            #reset backoff and retries
+                            back_of = 1
+                            retry = 1
+                except FatalError as e:
+                    logger.exception(e,f"-{func.__name__}")
+                    pipe_running = False
+                except AuthValidationError as e:
+                    logger.exception(e,f"-{func.__name__}")
+                    pipe_running = False
+                except TransientErrorBackoff as e:
+                    logger.warning(e,f"-{func.__name__}")
+                except TransientErrorRetry as e:
+                    if (retry < 1):
+                        pipe_running = False
+                        logger.warning(e,f"-{func.__name__}-pipeline stopped.")
+                    else:
+                        logger.warning(e,f"-{func.__name__}")
+
